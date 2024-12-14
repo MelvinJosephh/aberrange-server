610d976ea6d4f624c9e9d6876b0548664fe9cf1b
MelvinJosephh <collinsme.2000@gmail.com>
2024-12-14 20:14:27 +0300

    introduced authentication logic

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const contactRoutes = require("./routes/contact");
 const blogRoutes = require("./routes/blogsRoute"); // Import the blog routes
+const authRoutes = require ("./routes/login");
 require("dotenv").config();
 
 const app = express();
@@ -26,6 +27,7 @@ app.use(express.json());
 // Routes
 app.use("/api", contactRoutes);      // Contact routes
 app.use("/api/blogs", blogRoutes);    // Blog routes
+app.use("/api/auth", authRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
